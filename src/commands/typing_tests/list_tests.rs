2b7552b66443e5e03b533f3f9a7506154080bc95
Augustine Smith <absaugustineflash@gmail.com>
2026-01-21 21:57:13 +0000

    list typing tests(with println)

--- src/commands/typing_tests/list_tests.rs
+++ src/commands/typing_tests/list_tests.rs
@@ -1,10 +1,34 @@
 use std::fs;
+use serde::{Deserialize};
 
 use crate::errors::AppError;
+
+/// Represents a suite of typing tests loaded from a TOML file.
+#[derive(Deserialize, Debug)]
+pub struct TestSuite {
+    pub tests: Vec<TypingTest>,
+}
+
+/// Represents a single typing test.
+#[derive(Deserialize, Debug)]
+pub struct TypingTest {
+    /// Unique identifier for the typing test.
+    pub id: String,
+    /// Title of the typing test.
+    pub title: String,
+    /// Difficulty level of the typing test.
+    pub difficulty: String,
+    /// Content of the typing test.
+    pub content: String,
+    // TODO: Add tags
+}
 
 pub fn list_tests() -> Result<(), AppError> {
     let typing_tests_file = "./data/typing-tests/data.toml";
     let content = fs::read_to_string(std::path::Path::new(typing_tests_file))?;
-    println!("{}", content);
+    let suite: TestSuite = toml::from_str(&content)?;
+    for test in suite.tests {
+        println!("ID: {}, Title: {}, Difficulty: {}", test.id, test.title, test.difficulty);
+    }
     Ok(())
 }
--- src/errors.rs
+++ src/errors.rs
@@ -4,10 +4,19 @@ use thiserror::Error;
 pub enum AppError {
     #[error("IO error: {0}")]
     IO(std::io::Error),
+
+    #[error("TOML deserialization error: {0}")]
+    TomlDeserialization(toml::de::Error),
 }
 
 impl From<std::io::Error> for AppError {
     fn from(err: std::io::Error) -> Self {
         AppError::IO(err)
+    }
+}
+
+impl From<toml::de::Error> for AppError {
+    fn from(err: toml::de::Error) -> Self {
+        AppError::TomlDeserialization(err)
     }
 }
